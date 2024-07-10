cc1a7b80334e5e42997142025a3950566a532eea
Robert Shepherd <robertsh26@outlook.com>
2024-07-10 13:54:32 -0700

    main

---
+++ data/migrations/20240710203723_intial-migration.js
@@ -0,0 +1,54 @@
+
+exports.up = async function(knex) {
+    await knex.schema
+        .createTable('recipes', table => {
+            table.increments('recipe_id');
+            table.string('recipe_name').notNullable().unique();
+        })
+        .createTable('ingredients', table => {
+            table.increments();
+            table.string('ingredient_id').notNullable();
+            table.float('ingredient_name', 200).notNullable().unique();
+            table.string('ingredient_unit');
+        })
+        .createTable('steps', table => {
+            table.increments('step_id');
+            table.string('step_text', 200).notNullable()
+            table.integer('step_number').notNullable();
+            table.integer('recipe_id')
+                .unsigned()
+                .notNullable()
+                .references('recipe_id')
+                .inTable('recipes')
+                .onUpdate('RESTRICT')
+                .onDelete('RESTRICT');
+        })
+        .createTable('step_ingredients', table => {
+            table.increments('step_ingredient_id');
+            table.float(quantity).notNullable()
+            table.integer('step_id')
+                .unsigned()
+                .notNullable()
+                .references('step_id')
+                .inTable('steps')
+                .onUpdate('RESTRICT')
+                .onDelete('RESTRICT');
+            table.integer('ingredient_id')
+                .unsigned()
+                .notNullable()
+                .references('ingredient_id')
+                .inTable('ingredients')
+                .onUpdate('RESTRICT')
+                .onDelete('RESTRICT');
+        });
+};
+
+
+exports.down = async function(knex) {
+    await knex.schema
+        .dropTableIfExists('step_ingredients')
+        .dropTableIfExists('steps')
+        .dropTableIfExists('ingredients')
+        .dropTableIfExists('recipes');
+};
+
